56d076bcccc59aa76a070d89089b155a666b02ef
Usman Kabeer <uahmad565565@outlook.com>
2024-07-03 03:38:14 +0500

    clone solved problem

---
+++ IdentityServerAspNetIdentity/Services/ProfileService.cs
@@ -0,0 +1,67 @@
+﻿using IdentityModel;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using IdentityServerAspNetIdentity.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityServerAspNetIdentity.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IUserClaimsPrincipalFactory<ApplicationUser> _userClaimsPrincipalFactory;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public ProfileService(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IUserClaimsPrincipalFactory<ApplicationUser> userClaimsPrincipalFactory)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _userClaimsPrincipalFactory = userClaimsPrincipalFactory;
+        }
+
+        //The code above specifically search for the specific user and check whether that returned user data is assigned to any roles.
+        //And if it satisfy that condition then modify the current claims adding the role assigned for that user.
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            string sub = context.Subject.GetSubjectId();
+            ApplicationUser user = await _userManager.FindByIdAsync(sub);
+            ClaimsPrincipal userClaims = await _userClaimsPrincipalFactory.CreateAsync(user);
+
+            List<Claim> claims = userClaims.Claims.ToList();
+            claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
+            if (_userManager.SupportsUserRole)
+            {
+                IList<string> roles = await _userManager.GetRolesAsync(user);
+                foreach (string roleName in roles)
+                {
+                    claims.Add(new Claim(JwtClaimTypes.Role, roleName));
+                    if (_roleManager.SupportsRoleClaims)
+                    {
+                        IdentityRole role = await _roleManager.FindByNameAsync(roleName);
+                        if (role != null)
+                        {
+                            claims.AddRange(await _roleManager.GetClaimsAsync(role));
+                        }
+                    }
+                }
+            }
+
+            context.IssuedClaims = claims;
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            string sub = context.Subject.GetSubjectId();
+            ApplicationUser user = await _userManager.FindByIdAsync(sub);
+            context.IsActive = user != null;
+        }
+    }
+}
